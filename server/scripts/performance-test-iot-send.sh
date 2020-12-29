b6456560591cb8740fa48bacc3fda5a3079e4cb2
Iqbal Mohammad Abdul Ghoni <iqballmags@gmail.com>
2020-12-29 11:47:17 +0700

    Quick fix

--- server/scripts/performance-test-iot-send.sh
+++ server/scripts/performance-test-iot-send.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-for i in 100 200 300 400 500
+for i in 20 40 60 80 100
 do
   node bot clear --server http://10.42.0.1:8080 --secret-key skripsikomputer2;
   node bot create --server http://10.42.0.1:8080 --secret-key skripsikomputer2 $i;
+  sleep 5;
+  node bot attack-hit --server http://10.42.0.1:8080 --secret-key skripsikomputer2 $i 1;
+  echo "Done $i";
+  sleep 5;
+done
+
+for i in 120 140 160 180 200
+do
+  node bot clear --server http://10.42.0.1:8080 --secret-key skripsikomputer2;
+  node bot create --server http://10.42.0.1:8080 --secret-key skripsikomputer2 $i;
+  sleep 5;
   node bot attack-hit --server http://10.42.0.1:8080 --secret-key skripsikomputer2 $i 1;
   echo "Done $i";
   sleep 5;
